e22eced81d61094c94fdb9780bd701b28a6db1b6
praveen <gunnala1945@gmail.com>
2025-06-01 11:24:25 +0530

    change

---
+++ simplilearn/sample.js
@@ -0,0 +1,95 @@
+// console.log("welcome world")
+
+
+// let first_name="vs"
+// let last_name="code"
+// let full_name=first_name+last_name
+
+// console.log(full_name)
+
+// let result=true
+// console.log(result)
+// let age=19
+
+// if (age<18)
+//     console.log("Not eligible for voting")
+// if (age>18)
+//     console.log("Eligible for voting")
+
+
+// using a function method
+// function add(a,b)
+// {
+// return a+b;
+// }
+// // let result = add(2,3)
+// // console.log(result)
+// console.log(add(2,3))
+// console.log(add(1.5,1.5))
+// console.log(add("prav","een")) //by using strings
+
+// using "+" operator
+// let num1=5;
+// let num2=5
+// // let sum = num1+num2
+// // console.log(sum)
+
+
+// // adding more than 2 numbers
+// let num3=5
+// let total= num1+num2+num3
+// console.log(total)
+
+
+// // adding with assignment operator
+// let num4=5
+// num4+=5
+// console.log(num4)
+
+// function greet() {
+//             return ("good night")
+//         }
+//         console.log(greet())
+
+//  var employee= {
+//     name:"praveen", //key value pair
+//     salary:20000,
+//     age:25
+//  }
+//  console.log(employee)
+
+//  var greeting= {
+//     fullname: "praveen",
+//     message: "good morning",
+//     greet: (message,fullname)=>{console.log(message+"  "+fullname+"!!");}
+//  };
+//  console.log(greeting);
+
+// class student {
+//     constructor(student_name,rollnumber) {
+//         this.student_name = "praveen";
+//         this.rollnumber = 25;
+//     }
+// }
+// const stu1 = new student("Praveen",25);
+// console.log(stu1)
+
+
+function display(){
+ if(true){
+ var item1 = 'apple';        
+const item2 = 'ball';     
+let item3 = 'cloud';
+}
+ console.log(item1);
+ console.log(item2);
+ console.log(item3);
+ }
+
+ function display()
+ {
+    if (true){
+        var item1="apple";
+    }
+    console.log(item1);
+ }
