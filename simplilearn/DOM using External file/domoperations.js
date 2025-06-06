840f8640fad11fb220dd066ae275eeee70c6e98d
praveen <gunnala1945@gmail.com>
2025-06-06 12:11:30 +0530

    done

--- simplilearn/DOM using External file/domoperations.js
+++ simplilearn/DOM using External file/domoperations.js
@@ -3,11 +3,21 @@ function displayDomData(){
     // creating new tag like p,div,h1 etc.,
     var fname = document.getElementById("fname").value;
     var age = document.getElementById("age").value;
-    var pTag = document.createElement("p");            //<p></p>
+    if(fname.length==0 || age.length==0){
+        alert("please enter name or age")
+    }else{
+        var pTag = document.createElement("p");            //<p></p>
     var pTagContent = document.createTextNode("Welcome")          //Welcome content is ready
     var pTagContent=document.createTextNode("My name is "+fname+" & my age is "+age)  //content ready
     pTag.appendChild(pTagContent)              //<p>content which is ready</p>
+    if (age>=30){
+        pTag.setAttribute("class","abc")
+    }else{
+        pTag.setAttribute("class","xyz")
+    }
     document.getElementsByTagName("body")[0].appendChild(pTag)   //pTag is added to body tag
     document.getElementById("fname").value=" "
     document.getElementById("age").value=" "
+    }
+    
 }
